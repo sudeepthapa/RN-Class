fa63112ff79e9afb6cc387ccefa4d000d30e3be6
Sudip Thapa <sudeptech@gmail.com>
2021-02-15 19:03:09 +0545

    SrollView, ImageBackground, FlatList, SectionList

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, View, Text, ImageBackground, SectionList, FlatList } from 'react-native';
+import { FOODS } from './constants/data';
+
+const DATA = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"]
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"]
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"]
+  }
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <ScrollView horizontal style={styles.container} showsHorizontalScrollIndicator={false}>
+        {FOODS.map(food => {
+          return <ImageBackground key={food.title} style={[styles.box]} source={{ uri: food.image }}>
+            <Text style={styles.title}>{food.title}</Text>
+          </ImageBackground>
+        })
+        }
+      </ScrollView>
+
+      <SectionList
+        sections={DATA}
+        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+        keyExtractor={(item, index) => item + index}
+        renderSectionHeader={({ section }) => <Text style={styles.heading}> {section.title} </Text>}
+      />
+
+      <FlatList
+        data={FOODS}
+        horizontal
+        renderItem={({ item }) => <ImageBackground key={item.title} style={[styles.box]} source={{ uri: item.image }}>
+          <Text style={styles.title}>{item.title}</Text>
+        </ImageBackground>}
+        keyExtractor={(item, index) => item.title + index}
+      />
+
+    </ScrollView>
   );
 }
 
@@ -15,7 +57,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 30
+  },
+  box: {
+    height: 150,
+    width: 150,
+    marginRight: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 10
   },
+  image: {
+    height: '100%'
+  },
+  title: {
+    fontSize: 30,
+    color: '#fff'
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  item: {
+    fontSize: 14,
+    marginLeft: 30
+  }
 });
---
+++ constants/data.js
@@ -0,0 +1,37 @@
+const FOODS = [
+  {
+    title: 'Burger',
+    price:'$20',
+    description: 'this is a burger',
+    image: 'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    time: '20m',
+    rating:'4'
+  },
+  {
+    title: 'Sandwich',
+    price:'$30',
+    description: 'this is a sandwich',
+    image: 'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    time: '110m',
+    rating:'3'
+  },
+  {
+    title: 'Momo',
+    price:'$150',
+    description: 'this is a momo',
+    image: 'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    time: '10m',
+    rating:'5'
+  },
+  {
+    title: 'Sausage',
+    price:'$30',
+    description: 'this is a sausage',
+    image: 'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    time: '5m',
+    rating:'5'
+  },
+]
+const CATEGORIES = [];
+
+export { FOODS, CATEGORIES }
