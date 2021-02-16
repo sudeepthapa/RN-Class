8b8404e567169bc57f81a73995658afadc3ae5ca
Sudip Thapa <sudeptech@gmail.com>
2021-02-16 18:45:58 +0545

    TextInput, Alert, Modal

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, TextInput, Button, Text, Alert, Modal } from 'react-native';
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onSubmitHandler = () => {
+    Alert.alert(
+      'Do you want to submit?',
+      'Submitting this form will lead to login of user.',
+      [
+        {
+          text: "Cancel",
+          onPress: () => { },
+          style:"cancel"
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            console.log("You have successfully loggedIn")
+          }
+        }
+      ]
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        autoCapitalize="words"
+        placeholder="Username"
+        placeholderTextColor="#777"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Password"
+        placeholderTextColor="#777"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={onSubmitHandler} />
+      <Button title="OpenModal" onPress={()=>setIsOpen(true)} />
+      <Modal visible={isOpen} transparent animationType="fade" statusBarTranslucent>
+        <View style={styles.modalView}>
+          <View style={styles.modalBody}>
+            <Text>Hello </Text>
+            <Text>Hello1 </Text>
+            <Text>Hello 2</Text>
+            <Text>Hello 3</Text>
+            <Text>Hello 4</Text>
+            <Text>Hello 55</Text>
+            <Button title="CloseModal" onPress={()=>setIsOpen(false)} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderColor: '#777',
+    borderWidth: 1,
+    width: 300,
+    height:40,
+    paddingHorizontal: 15,
+    marginBottom:20
+  },
+  modalView: {
+    flex:1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalBody: {
+    padding:30,
+    backgroundColor:'red'
+  }
 });
