2cc13af1e493dc44c52aba33e23440f5bdc4274a
Sudip Thapa <sudeptech@gmail.com>
2021-02-22 19:00:35 +0545

    NATIVE BASE INTEGRATION

--- App.js
+++ App.js
@@ -1,45 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Platform, Dimensions } from 'react-native';
+import { StyleSheet, View, Platform, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
+import { Container, Header, Content, Button, Text, Spinner } from 'native-base';
 
-const BodyText = (props) => {
-  return <Text style={{fontFamily:'Inter_400Regular', fontSize:14}}>{ props.children }</Text>
-}
-const H1 = (props) => {
-  return <Text style={{fontFamily:'ubuntu-regular', fontSize:24}}>{ props.children }</Text>
-}
+// const BodyText = (props) => {
+//   return <Text style={{fontFamily:'Inter_400Regular', fontSize:14}}>{ props.children }</Text>
+// }
+// const H1 = (props) => {
+//   return <Text style={{fontFamily:'ubuntu-regular', fontSize:24}}>{ props.children }</Text>
+// }
 
 import {
   useFonts,
   Inter_700Bold,
   Inter_400Regular
 } from '@expo-google-fonts/inter';
+import { loadAsync } from 'expo-font';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Inter_700Bold,
-    Inter_400Regular,
-    // @ts-ignore
-    'ubuntu-regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
-    // @ts-ignore
-    'ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
-  });
-  if (!fontsLoaded) {
-    return <Text>LOADING</Text>
+  const [isReady, setIsReady] = React.useState(false)
+
+  const loadFont = async () => {
+    await loadAsync({
+      Inter_700Bold,
+      Inter_400Regular,
+      // @ts-ignore
+      'ubuntu-regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
+      // @ts-ignore
+      'ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
+      // @ts-ignore
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      // @ts-ignore
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font
+    })
+    setIsReady(true)
+  }
+
+  React.useEffect(() => {
+    loadFont()
+  }, [])
+  
+  if (!isReady) {
+    return <Button><Text>Loading</Text></Button>
   }
   return (
-    <View style={styles.container}>
-      <BodyText>Open up App.js to start working on your app!</BodyText>
-      <StatusBar style="auto" />
-      <Ionicons name={Platform.OS === 'android' ? 'md-add' : 'ios-add'} />
-      <H1>
-        {JSON.stringify(Dimensions.get('screen'))}
-      </H1>
-      <Text>
-        {JSON.stringify(Dimensions.get('window'))}
-      </Text>
-    </View>
+    <Container>
+        <Header />
+        <Content>
+          <Button bordered light>
+            <Text>Light</Text>
+          </Button>
+          <Button bordered>
+            <Text>Primary</Text>
+          </Button>
+          <Button bordered success>
+            <Text>Success</Text>
+          </Button>
+          <Button bordered info>
+            <Text>Info</Text>
+          </Button>
+          <Button bordered warning>
+            <Text>Warning</Text>
+          </Button>
+          <Button bordered danger>
+            <Text>Danger</Text>
+          </Button>
+          <Button bordered dark>
+            <Text>Dark</Text>
+        </Button>
+        <Spinner />
+          <Spinner color='green' />
+          <Spinner color='blue' />
+        </Content>
+      </Container>
   );
 }
 
