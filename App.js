6a274998cbcc542b0512d4058400c67d45ade55c
Sudip Thapa <sudeptech@gmail.com>
2021-02-25 17:30:58 +0545

    font loading fix

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons'
 import { Spinner } from 'native-base';
+import AppLoading from 'expo-app-loading'
 
 // const BodyText = (props) => {
 //   return <Text style={{fontFamily:'Inter_400Regular', fontSize:14}}>{ props.children }</Text>
@@ -16,35 +17,30 @@ import {
 import { loadAsync } from 'expo-font';
 import MainNavigator from './navigation/MainNavigator';
 
+const loadFont =  () => {
+  return loadAsync({
+    Inter_700Bold,
+    Inter_400Regular,
+    // @ts-ignore
+    'ubuntu-regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
+    // @ts-ignore
+    'ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
+    // @ts-ignore
+    Roboto: require('native-base/Fonts/Roboto.ttf'),
+    // @ts-ignore
+    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    ...Ionicons.font
+  })
+}
+
 export default function App() {
   const [isReady, setIsReady] = React.useState(false)
 
-  const loadFont = async () => {
-    await loadAsync({
-      Inter_700Bold,
-      Inter_400Regular,
-      // @ts-ignore
-      'ubuntu-regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
-      // @ts-ignore
-      'ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
-      // @ts-ignore
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      // @ts-ignore
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font
-    })
-    setIsReady(true)
-  }
-
-  React.useEffect(() => {
-    loadFont()
-  }, [])
-  
   if (!isReady) {
-    return <Spinner color='green' />
-  } else {    
-    return (
-      <MainNavigator />
-    );
-  }
+    return <AppLoading onError={(err)=>console.log(err)} startAsync={loadFont} onFinish={()=>setIsReady(true)} />
+  }   
+  return (
+    <MainNavigator />
+  );
+  
 }
